3ce92b95d20200fbf9a4382a6b4300686545d37c
chrono <chrono059@gmail.com>
2022-12-02 01:10:56 +0300

    простая многопоточность

--- app/src/main/java/homework/TagirLock.java
+++ app/src/main/java/homework/TagirLock.java
@@ -1,4 +1,27 @@
 package homework;
 
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.stream.Stream;
+
 public class TagirLock {
+    private static int counter = 0;
+
+    public static void main(String[] args) {
+        Runnable action = () -> {
+            for (int i = 0; i < 10; i++) {
+                counter++;
+            }
+        };
+
+        ExecutorService executor = Executors.newScheduledThreadPool(10);
+        var runnableStream = Stream.generate(() -> action)
+                .limit(1000)
+                .map(runnable -> CompletableFuture.runAsync(runnable, executor))
+                .toArray(CompletableFuture[]::new);
+        CompletableFuture.allOf(runnableStream).join();
+        executor.shutdown();
+        System.out.println(counter + ";" + Thread.currentThread().getId());
+    }
 }
