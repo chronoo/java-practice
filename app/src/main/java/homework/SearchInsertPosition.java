aee28121f44569ab0038cf565d02e836391cf761
chrono <chrono059@gmail.com>
2022-12-15 01:31:57 +0300

    Search Insert Position. Up

--- app/src/main/java/homework/SearchInsertPosition.java
+++ app/src/main/java/homework/SearchInsertPosition.java
@@ -4,11 +4,12 @@ public class SearchInsertPosition {
     public int searchInsert(int[] nums, int target) {
         int l = 0;
         int r = nums.length - 1;
-        while (l < r) {
-            int med = (l + r) / 2;
-            if (target <= nums[med]) r = med;
-            else l = med + 1;
+        while (l <= r) {
+            int med = l + (r - l) / 2;
+            if (target == nums[med]) return med;
+            if (target > nums[med]) l = med + 1;
+            else r = med - 1;
         }
-        return nums[l] >= target ? l : l + 1;
+        return l;
     }
 }
