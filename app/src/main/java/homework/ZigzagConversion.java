6ad780e804ea6bdc6d09d40126c94873afbab42c
chrono <chrono059@gmail.com>
2022-12-20 02:09:05 +0300

    Zigzag Conversion. Up

--- app/src/main/java/homework/ZigzagConversion.java
+++ app/src/main/java/homework/ZigzagConversion.java
@@ -2,6 +2,28 @@ package homework;
 
 public class ZigzagConversion {
     public String convert(String s, int numRows) {
+        StringBuilder sb = new StringBuilder();
+        int circle = numRows + numRows - 2;
+        int interval = circle == 0 ? 1 : circle;
+
+        for (int rowIndex = 0; rowIndex < numRows; rowIndex++) {
+            for (int current = rowIndex; current < s.length(); current += interval) {
+                sb.append(s.charAt(current));
+
+                boolean isFirst = rowIndex == 0;
+                boolean isLast = rowIndex == numRows - 1;
+                int twoHalfStepBack = rowIndex * 2;
+                int tailIndex = current + interval - twoHalfStepBack;
+                if (!isFirst && !isLast && tailIndex < s.length()) {
+                    sb.append(s.charAt(tailIndex));
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+
+    public String convert1(String s, int numRows) {
         if (numRows == 1) return s;
         int hMin = numRows - 2;
         int circle = numRows + hMin;
