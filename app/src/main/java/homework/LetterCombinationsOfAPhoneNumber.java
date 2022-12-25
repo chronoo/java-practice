868120f175ab6db8ecf62a4161440cb14fe158f1
chrono <chrono059@gmail.com>
2022-12-26 02:43:50 +0300

    Letter Combinations Of A Phone Number. Up

--- app/src/main/java/homework/LetterCombinationsOfAPhoneNumber.java
+++ app/src/main/java/homework/LetterCombinationsOfAPhoneNumber.java
@@ -3,21 +3,33 @@ package homework;
 import java.util.*;
 
 public class LetterCombinationsOfAPhoneNumber {
+    private static final Map<Character, List<Character>> KEYS = Map.of(
+            '2', List.of('a', 'b', 'c'),
+            '3', List.of('d', 'e', 'f'),
+            '4', List.of('g', 'h', 'i'),
+            '5', List.of('j', 'k', 'l'),
+            '6', List.of('m', 'n', 'o'),
+            '7', List.of('p', 'q', 'r', 's'),
+            '8', List.of('t', 'u', 'v'),
+            '9', List.of('w', 'x', 'y', 'z')
+    );
+
     public List<String> letterCombinations(String digits) {
-        LinkedList<String> ans = new LinkedList<>();
-        if (digits.isEmpty()) return ans;
-        String[] mapping = new String[]{"0", "1", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"};
-        ans.add("");
-        for (int i = 0; i < digits.length(); i++) {
-            int currentNumber = Character.getNumericValue(digits.charAt(i));
-            while (ans.peek().length() == i) {
-                String top = ans.remove();
-                char[] currentKeys = mapping[currentNumber].toCharArray();
-                for (char currentKey : currentKeys) {
-                    ans.add(top + currentKey);
-                }
-            }
+        LinkedList<String> answer = new LinkedList<>();
+        if (digits.isEmpty()) return answer;
+        String currentAnswer = "";
+        generate(currentAnswer, answer, digits, 0);
+        return answer;
+    }
+
+    private void generate(String current, LinkedList<String> answer, String digits, int level) {
+        if (level == digits.length()) {
+            answer.add(current);
+            return;
         }
-        return ans;
+
+        for (Character key : KEYS.get(digits.charAt(level))) {
+            generate(current + key, answer, digits, level+1);
+        }
     }
 }
