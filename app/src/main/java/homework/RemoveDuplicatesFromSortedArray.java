5c38c278f0d343b5521049ef7755d45a25153982
chrono <chrono059@gmail.com>
2023-04-24 00:43:15 +0300

    Remove Duplicates from Sorted Array. up

--- app/src/main/java/homework/RemoveDuplicatesFromSortedArray.java
+++ app/src/main/java/homework/RemoveDuplicatesFromSortedArray.java
@@ -1,10 +1,17 @@
 package homework;
 
-import java.util.Arrays;
 public class RemoveDuplicatesFromSortedArray {
     public int removeDuplicates(int[] nums) {
-        int[] uniqueNums = Arrays.stream(nums).distinct().toArray();
-        System.arraycopy(uniqueNums, 0, nums, 0, uniqueNums.length);
-        return uniqueNums.length;
+        int currentNum = nums[0];
+        int currentNumIndex = 1;
+        for (int i = 1; i < nums.length; i++) {
+            int tempNum = nums[i];
+            if (tempNum > currentNum) {
+                nums[currentNumIndex] = tempNum;
+                currentNumIndex++;
+                currentNum = tempNum;
+            }
+        }
+        return currentNumIndex;
     }
 }
