63f3ac856671f16f2f99aab99f49858b4d5fa874
chrono <chrono059@gmail.com>
2023-05-27 00:14:30 +0300

    Number of Substrings Containing All Three Characters. up

--- app/src/main/java/homework/NumberOfSubstringsContainingAllThreeCharacters.java
+++ app/src/main/java/homework/NumberOfSubstringsContainingAllThreeCharacters.java
@@ -1,27 +1,13 @@
 package homework;
 
-import java.util.IntSummaryStatistics;
-import java.util.stream.IntStream;
-
 public class NumberOfSubstringsContainingAllThreeCharacters {
     public int numberOfSubstrings(String s) {
         int count = 0;
-        if (getStatistics(s, 0).getMin() == -1) {
-            return 0;
-        }
-        for (int i = 0; i < s.length() - 2; i++) {
-            var stat = getStatistics(s, i);
-            if (stat.getMin() > -1)
-                count += s.length() - stat.getMax();
+        int[] positions = {-1, -1, -1};
+        for (int i = 0; i < s.length(); i++) {
+            positions[s.charAt(i) - 'a'] = i;
+            count += Math.min(positions[0], Math.min(positions[1], positions[2])) + 1;
         }
         return count;
-    }
-
-    private IntSummaryStatistics getStatistics(String s, int start) {
-        int firstA = s.indexOf('a', start);
-        int firstB = s.indexOf('b', start);
-        int firstC = s.indexOf('c', start);
-        return IntStream.of(firstA, firstB, firstC)
-            .summaryStatistics();
     }
 }
