c53594de5246f3a0ab4937e5fdc7f8e072f3c5b5
chrono <chrono059@gmail.com>
2023-06-01 00:11:03 +0300

    Max Consecutive Ones III. up

--- app/src/main/java/homework/MaxConsecutiveOnesIII.java
+++ app/src/main/java/homework/MaxConsecutiveOnesIII.java
@@ -1,33 +1,15 @@
 package homework;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class MaxConsecutiveOnesIII {
     public int longestOnes(int[] nums, int k) {
-        List<Integer> zeros = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            int num = nums[i];
-            if (num == 0) {
-                zeros.add(i);
+        int left = 0, right = 0;
+        for (; left < nums.length ; left++) {
+            if (nums[left] == 0) k--;
+            if (k < 0) {
+                if (nums[right] == 0) k++;
+                right++;
             }
         }
-        if (zeros.size() <= k) return nums.length;
-        int max = 0;
-        for (int i = 0; i <= zeros.size() - k; i++) {
-            List<Integer> copy = new ArrayList<>();
-            copy.add(-1);
-            for (int j = 0; j < zeros.size(); j++) {
-                if (j < i || j > i + k - 1) {
-                    copy.add(zeros.get(j));
-                }
-            }
-            copy.add(nums.length);
-            for (int j = 0; j < copy.size() - 1; j++) {
-                int sum = copy.get(j + 1) - copy.get(j);
-                if (sum > max) max = sum - 1;
-            }
-        }
-        return max;
+        return left - right;
     }
 }
