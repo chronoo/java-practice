88d5fe35a02e20272dac976dfbca3e8f09a4d163
chrono <chrono059@gmail.com>
2023-06-01 23:47:05 +0300

    Count Number of Nice Subarrays. up

--- app/src/main/java/homework/CountNumberOfNiceSubarrays.java
+++ app/src/main/java/homework/CountNumberOfNiceSubarrays.java
@@ -2,19 +2,13 @@ package homework;
 
 public class CountNumberOfNiceSubarrays {
     public int numberOfSubarrays(int[] nums, int k) {
-        int i = 0, odd = 0, count = 0, res = 0;
-        for (int num : nums) {
-            if (num % 2 == 1) {
-                odd++;
-                count = 0;
-            }
-            for (; odd == k; i++, count++) {
-                if (nums[i] % 2 == 1) {
-                    odd--;
-                }
-            }
-            res += count;
+        int count = 0, ans = 0;
+        int[] arr = new int[nums.length];
+        for (int x : nums) {
+            arr[count]++;
+            if (x % 2 == 1) count++;
+            if (count >= k) ans += arr[count - k];
         }
-        return res;
+        return ans;
     }
 }
