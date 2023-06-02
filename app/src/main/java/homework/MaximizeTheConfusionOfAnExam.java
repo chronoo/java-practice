2b7d2a5381566efef17da3e4878b5d694ed4e5b9
chrono <chrono059@gmail.com>
2023-06-02 22:55:07 +0300

    Maximize the Confusion of an Exam. up

--- app/src/main/java/homework/MaximizeTheConfusionOfAnExam.java
+++ app/src/main/java/homework/MaximizeTheConfusionOfAnExam.java
@@ -2,21 +2,20 @@ package homework;
 
 public class MaximizeTheConfusionOfAnExam {
     public int maxConsecutiveAnswers(String answerKey, int k) {
-        int left=0;
-        int trues=0;
-        int falses=0;
-        int max=0;
-        for(int right=0; right<answerKey.length(); right++) {
-            char chR = answerKey.charAt(right);
-            if(chR == 'T') trues++;
+        int max = 0;
+        int trues = 0;
+        int falses = 0;
+        int left = 0;
+        for (int right = 0; right < answerKey.length(); right++) {
+            if (answerKey.charAt(right) == 'T') trues++;
             else falses++;
-            while(Math.min(trues, falses) > k) {
-                char chL = answerKey.charAt(left);
-                if(chL == 'T') trues--;
+            while (trues > k && falses > k) {
+                if (answerKey.charAt(left) == 'T') trues--;
                 else falses--;
                 left++;
             }
-            max = Math.max(max, right-left+1);
+            int currentMax = right - left + 1;
+            if (currentMax > max) max = currentMax;
         }
         return max;
     }
--- app/src/test/java/homework/MaximizeTheConfusionOfAnExamTest.java
+++ app/src/test/java/homework/MaximizeTheConfusionOfAnExamTest.java
@@ -3,7 +3,6 @@ package homework;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
 
 class MaximizeTheConfusionOfAnExamTest {
 
