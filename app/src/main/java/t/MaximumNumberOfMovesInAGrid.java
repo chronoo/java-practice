1d4aa419de046d0faeb94557cc231d217b8c2e21
chrono <chrono059@gmail.com>
2024-10-29 18:30:31 +0300

    2684. Maximum Number of Moves in a Grid. Up

--- app/src/main/java/t/MaximumNumberOfMovesInAGrid.java
+++ app/src/main/java/t/MaximumNumberOfMovesInAGrid.java
@@ -6,37 +6,29 @@ import java.util.concurrent.atomic.AtomicInteger;
  * https://leetcode.com/problems/maximum-number-of-moves-in-a-grid
  */
 public class MaximumNumberOfMovesInAGrid {
+    int ans = 0;
+    Integer[][] dp;
     public int maxMoves(int[][] grid) {
-        if (grid.length == 0) return grid[0].length;
-        var max = new AtomicInteger(0);
-        for (int x = 0; x < grid.length; x++) {
-            proc(grid, max, x, 0, 0);
+        int n = grid.length;
+        int m = grid[0].length;
+        dp = new Integer[n + 1][m + 1];
+        for (int i = 0; i < n; i++) {
+            ans = Math.max(ans, dfs(grid, i, 0, 0) - 1);
         }
-        return max.get();
+        return ans;
     }
 
-    private void proc(int[][] grid, AtomicInteger max, int x, int y, int depth) {
-        int cur = grid[x][y];
-        if (depth > max.get()) {
-            max.set(depth);
+    int dfs(int[][] grid, int x, int y, int prev) {
+        int n = grid.length;
+        int m = grid[0].length;
+        if (x < 0 || x == n || y < 0 || y == m || grid[x][y] <= prev) {
+            return 0;
         }
-        if (y + 1 < grid[0].length) {
-            if (x - 1 >= 0) {
-                int next1 = grid[x-1][y + 1];
-                if (next1 > cur) {
-                    proc(grid, max, x-1, y + 1, depth + 1);
-                }
-            }
-            if (x + 1 < grid.length) {
-                int next2 = grid[x+1][y + 1];
-                if (next2 > cur) {
-                    proc(grid, max, x+1, y + 1, depth + 1);
-                }
-            }
-            int next3 = grid[x][y + 1];
-            if (next3 > cur) {
-                proc(grid, max, x, y + 1, depth + 1);
-            }
-        }
+        if (dp[x][y] != null) return dp[x][y];
+        int val = grid[x][y];
+        int a  = 1  + dfs(grid, x - 1, y + 1,val);
+        int b = 1 + dfs(grid, x + 1, y + 1,val);
+        int c = 1 + dfs(grid, x, y + 1,val);
+        return dp[x][y] = Math.max(a, Math.max(b, c));
     }
 }
