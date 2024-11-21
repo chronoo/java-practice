a61fa30da94c3cab3d655a075ed32a1b282d2b5d
chrono <chrono059@gmail.com>
2024-11-21 20:02:46 +0300

    190. Reverse Bits. Up

--- app/src/main/java/t/ReverseBits.java
+++ app/src/main/java/t/ReverseBits.java
@@ -5,8 +5,6 @@ package t;
  */
 public class ReverseBits {
     public int reverseBits(int n) {
-        var sb = new StringBuilder(Integer.toBinaryString(n)).reverse();
-        var zeros = 32 - sb.length();
-        return Integer.parseUnsignedInt(sb.repeat("0", zeros).toString(), 2);
+        return Integer.reverse(n);
     }
 }
