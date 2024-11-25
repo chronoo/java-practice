b94222c1af42ea08f61bc79f1d6e8e8d73482d91
chrono <chrono059@gmail.com>
2024-11-25 20:50:46 +0300

    263. Ugly Number. Up

--- app/src/main/java/t/UglyNumber.java
+++ app/src/main/java/t/UglyNumber.java
@@ -5,11 +5,13 @@ package t;
  */
 public class UglyNumber {
     public boolean isUgly(int n) {
-        if (n == 0) return false;
-        if(n > 0 && n <= 3) return true;
-        if (n % 2 == 0) return isUgly(n / 2);
-        if (n % 3 == 0) return isUgly(n / 3);
-        if (n % 5 == 0) return isUgly(n / 5);
-        return false;
+        while (true) {
+            if (n == 0) return false;
+            if (n > 0 && n <= 3) return true;
+            if (n % 2 == 0) n /= 2;
+            else if (n % 3 == 0) n /= 3;
+            else if (n % 5 == 0) n /= 5;
+            else return false;
+        }
     }
 }
