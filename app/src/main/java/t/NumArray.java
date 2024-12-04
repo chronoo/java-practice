92825070a6a017d5e409ca0683e9fdc98dcdb851
chrono <chrono059@gmail.com>
2024-12-04 21:21:20 +0300

    303. Range Sum Query - Immutable. Up

--- app/src/main/java/t/NumArray.java
+++ app/src/main/java/t/NumArray.java
@@ -4,17 +4,18 @@ package t;
  * https://leetcode.com/problems/range-sum-query-immutable
  */
 public class NumArray {
-    private final int[] nums;
+    private final int[] sums;
 
     public NumArray(int[] nums) {
-        this.nums = nums;
+        sums = new int[nums.length];
+        var sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+            sums[i] = sum;
+        }
     }
 
     public int sumRange(int left, int right) {
-        var sum = 0;
-        for (int i = left; i <= right; i++) {
-            sum += nums[i];
-        }
-        return sum;
+        return left == 0 ? sums[right] : sums[right] - sums[left - 1];
     }
 }
