1f4c46347dd59694c38ae54b9946a33fb9c8da48
chrono <chrono059@gmail.com>
2024-12-05 21:59:42 +0300

    326. Power of Three. Up

--- app/src/main/java/t/PowerOfThree.java
+++ app/src/main/java/t/PowerOfThree.java
@@ -7,8 +7,10 @@ public class PowerOfThree {
     public boolean isPowerOfThree(int n) {
         if (n < 1) return false;
         if (n == 1) return true;
-        if (n == 3) return true;
-        if (n % 3 != 0) return false;
-        return isPowerOfThree(n / 3);
+        while (n > 1) {
+            if (n % 3 != 0) return false;
+            n /= 3;
+        }
+        return true;
     }
 }
