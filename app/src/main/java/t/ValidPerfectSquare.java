b6ab083348d6e28646752064240198a4e1948425
chrono <chrono059@gmail.com>
2024-12-08 16:00:53 +0300

    367. Valid Perfect Square. Up

--- app/src/main/java/t/ValidPerfectSquare.java
+++ app/src/main/java/t/ValidPerfectSquare.java
@@ -7,13 +7,13 @@ public class ValidPerfectSquare {
     public boolean isPerfectSquare(int num) {
         var left = 1;
         var right = num;
-        while (right > left) {
+        while (left <= right) {
             var med = left + (right - left) / 2;
             var square = (long) med * med;
             if (square > num) right = med -1;
             else if (square < num) left = med + 1;
             else return true;
         }
-        return right * right == num;
+        return false;
     }
 }
