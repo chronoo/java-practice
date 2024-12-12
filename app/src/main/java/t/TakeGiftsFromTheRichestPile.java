e0146269e86f9487fc4974a6cde9e2a38604f6f9
chrono <chrono059@gmail.com>
2024-12-12 21:07:35 +0300

    2558. Take Gifts From the Richest Pile. Up

--- app/src/main/java/t/TakeGiftsFromTheRichestPile.java
+++ app/src/main/java/t/TakeGiftsFromTheRichestPile.java
@@ -1,6 +1,7 @@
 package t;
 
-import java.util.*;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 
 /**
  * https://leetcode.com/problems/take-gifts-from-the-richest-pile
@@ -9,12 +10,16 @@ public class TakeGiftsFromTheRichestPile {
     public long pickGifts(int[] gifts, int k) {
         var queue = new PriorityQueue<>(Comparator.<Integer>reverseOrder());
         for (var gift : gifts) {
-           queue.offer(gift);
+            queue.offer(gift);
         }
         for (var i = 0; i < k; i++) {
             var max = queue.poll();
             queue.offer((int) Math.sqrt(max));
         }
-        return queue.stream().mapToLong(x -> x).sum();
+        var sum = 0L;
+        for (var element : queue) {
+            sum += element;
+        }
+        return sum;
     }
 }
