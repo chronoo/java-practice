6b7a6a4662d1398a0fc4e3f9c116a2b4ea9fee85
chrono <chrono059@gmail.com>
2024-12-31 17:08:40 +0300

    441. Arranging Coins. Up

--- app/src/main/java/t/ArrangingCoins.java
+++ app/src/main/java/t/ArrangingCoins.java
@@ -5,15 +5,6 @@ package t;
  */
 public class ArrangingCoins {
     public int arrangeCoins(int n) {
-        var left = 0L;
-        var right = (long) n;
-        while (left <= right) {
-            var middle = left + (right - left) / 2;
-            var summ = middle * (middle + 1) / 2;
-            if (summ < n) left = middle + 1;
-            else if (summ > n) right = middle - 1;
-            else return (int) middle;
-        }
-        return (int) right;
+        return (int) ((Math.sqrt(1 + (long) 8 * n) - 1) / 2);
     }
 }
