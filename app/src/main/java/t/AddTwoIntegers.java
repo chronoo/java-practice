78c04ba20d69ff32f63e55b15f1c2277ea63a785
chrono <chrono059@gmail.com>
2025-02-23 20:35:57 +0300

    2235. Add Two Integers

---
+++ app/src/main/java/t/AddTwoIntegers.java
@@ -0,0 +1,10 @@
+package t;
+
+/**
+ * https://leetcode.com/problems/add-two-integers/description
+ */
+public class AddTwoIntegers {
+    public int sum(int num1, int num2) {
+        return num1 + num2;
+    }
+}
