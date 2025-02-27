2fbaee103f8eaea813cc68109d1bc54578ab30c6
chrono <chrono059@gmail.com>
2025-02-27 20:56:28 +0300

    2769. Find the Maximum Achievable Number

---
+++ app/src/main/java/t/FindTheMaximumAchievableNumber.java
@@ -0,0 +1,10 @@
+package t;
+
+/**
+ * https://leetcode.com/problems/find-the-maximum-achievable-number
+ */
+public class FindTheMaximumAchievableNumber {
+    public int theMaximumAchievableX(int num, int t) {
+        return num + 2 * t;
+    }
+}
