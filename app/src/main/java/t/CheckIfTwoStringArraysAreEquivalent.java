97952536dfba28598e7d0cf1ce22ba0eca521ad4
chrono <chrono059@gmail.com>
2025-04-15 19:24:53 +0300

    1662. Check If Two String Arrays are Equivalent

---
+++ app/src/main/java/t/CheckIfTwoStringArraysAreEquivalent.java
@@ -0,0 +1,7 @@
+package t;
+
+public class CheckIfTwoStringArraysAreEquivalent {
+    public boolean arrayStringsAreEqual(String[] word1, String[] word2) {
+        return String.join("", word1).equals(String.join("", word2));
+    }
+}
