90c146e46d6330793714c119eadc76884c4e97d2
chrono <chrono059@gmail.com>
2025-05-24 20:58:05 +0300

    2843. Count Symmetric Integers. Up

--- app/src/main/java/t/CountSymmetricIntegers.scala
+++ app/src/main/java/t/CountSymmetricIntegers.scala
@@ -1,22 +1,14 @@
 package t
 
 object CountSymmetricIntegers {
-  def countSymmetricIntegers(low: Int, high: Int): Int = {
-    var i = 0
-    var count = 0
-    for
-      i <- low to high
-    do
-      if valid(i) then
-        count +=1
-    count
-  }
+  def countSymmetricIntegers(low: Int, high: Int): Int =
+    (low to high).count(symmetric)
 
-  private def valid(i: Int): Boolean = {
-    val str = i.toString.toCharArray
-    if str.length%2 == 1 then
+  private def symmetric(x: Int): Boolean = {
+    val str = x.toString.toCharArray
+    if str.length % 2 == 1 then
       false
     else
-      str.slice(0, str.length/2).sum == str.slice(str.length/2, str.length).sum
+      str.slice(0, str.length / 2).sum == str.slice(str.length / 2, str.length).sum
   }
 }
